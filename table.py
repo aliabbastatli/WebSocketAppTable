d7e1e0ec2cf5c9b7786802e6e7c04c9dc1b8f358
AliAbbasTatli <aliabbastatli@yandex.com>
2021-01-08 12:46:40 +0300

    django-channels commmits

---
+++ table.py
@@ -0,0 +1,12 @@
+import websocket,json,random
+ws = websocket.WebSocket()
+ws.connect('ws://localhost:8000/ws/tableData/')
+listOfindex=['stock1','stock2','stock3','stock4','stock5']
+for i in range(100):
+    import time
+    time.sleep(1)
+    pp = json.dumps({'indexName':random.choice(listOfindex)
+                     ,'value':random.randint(1,1000)})
+    ws.send(pp)
+
+random.choice(listOfindex)
